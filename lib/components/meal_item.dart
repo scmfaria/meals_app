b0926e18d2b4eded9001e2bf15676136e4bdecbc
scmfaria <sara.moura.faria@gmail.com>
2020-12-17 03:43:02 -0300

    add categories meals screen

---
+++ lib/components/meal_item.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+
+  const MealItem(this.meal);
+
+  void _selectMeal() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            // esse componente Stack aceita um elemento por cima do outro
+            Stack(
+              children: [
+                // componente para colocar borda 
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                // esse widget Positioned é para ancorar o que esta dentro dele, exemplo: bottom and right (define a posicao do elemento) 
+                Positioned(
+                  bottom: 10,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('${meal.duration} min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text(meal.complexityText),  
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text(meal.costText),  
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
